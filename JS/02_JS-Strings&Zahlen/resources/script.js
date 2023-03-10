8807586d9cca57b3bec1d952fb6059ef00a47966
AaronHtlDornbirn <91245441+AaronHtlDornbirn@users.noreply.github.com>
2023-03-10 14:15:55 +0100

    02_gemacht

---
+++ JS/02_JS-Strings&Zahlen/resources/script.js
@@ -0,0 +1,34 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+console.log(ab);
+
+// +, +, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+let result = c + d;
+
+console.log(result);
+
+//Texte Strings
+
+let name = "Aaron";
+console.log(name);
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab)
+
+//Aufgaben
+let x = 17.4
+let y = 3.9
+
+let xy = x / y;
+
+console.log("The result of " + x + " divided by " + y + " is " + xy)
+
+let firstname = "Aaron";
+let lastname = "Haltmeier";
+
+console.log("Ich heiße " + firstname + " " + lastname)
