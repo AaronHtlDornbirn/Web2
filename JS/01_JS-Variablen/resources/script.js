50c616811377d0edfd497a955b2b46725cead246
AaronHtlDornbirn <91245441+AaronHtlDornbirn@users.noreply.github.com>
2023-03-10 13:47:27 +0100

    01_gemacht

---
+++ JS/01_JS-Variablen/resources/script.js
@@ -0,0 +1,41 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+/*
+//Variablen
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den Wert " + a);
+
+a = 10;
+console.log("A hat den Wert " + a);
+
+console.log("a + b = " + c)
+
+//Rechnungsarten
+// *, /, +, -
+
+c = b - a;
+console.log("b - a = " + c)
+*/
+//Aufgaben:
+let a = 7;
+let b = 8;
+let c = 10;
+
+console.log("a + b + c = " + a + b + c)
+console.log("(a + b) / c = " + (a + b) / c)
+console.log("c - a + b = " + c - a + b)
+console.log("a + b + c * 3 = " + a + b + c * 3)
+console.log("(a + b + c) / 4 = " + (a + b + c) / 4)
+
+a = 3;
+b = a + 3;
+c = c - 3;
+
+console.log("a + b + c = " + a + b + c)
+console.log("(a + b) / c = " + (a + b) / c)
+console.log("c - a + b = " + c - a + b)
+console.log("a + b + c * 3 = " + a + b + c * 3)
+console.log("(a + b + c) / 4 = " + (a + b + c) / 4)
