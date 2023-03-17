89a9ac43330f336686da44a467ed01f5fef90bba
AaronHtlDornbirn <91245441+AaronHtlDornbirn@users.noreply.github.com>
2023-03-17 14:49:28 +0100

    04_gemacht

---
+++ JS/04_Schleifen/resources/script.js
@@ -0,0 +1,45 @@
+console.log("test")
+
+//Schleife
+
+for (let count = 10; count >= 0; count--) {
+    console.log(count + " Aaron")
+}
+
+console.log("fertig")
+
+// Zähle von 7 bis 45
+for (let count1 = 7; count1 <= 45; count1++) {
+    console.log(count1 + " Eins")
+}
+
+// Zähle von 9 bis 99
+for (let count2 = 9; count2 <= 99; count2++) {
+    console.log(count2 + " Zwei")
+}
+
+// Zähle rückwärts von 10 bis 0
+for (let count3 = 10; count3 >= 0; count3--) {
+    console.log(count3 + " Drei")
+}
+
+// Zähle rückwärts von 1234 bis 1207
+for (let count4 = 1234; count4 >= 1207; count4--) {
+    console.log(count4 + " Vier")
+}
+
+// Gib Deinen Namen genau 8 mal aus
+for (let count5 = 1; count5 <= 8; count5++) {
+    console.log(count5 + "Aaron")
+}
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (let count7 = 0; count7 <= 10; count7++) {
+    for (let count7 = 0; count7 <= 10; count7++) {
+        console.log(count7)
+    }
+    console.log("Aaron")
+    for (let count9 = 10; count9 >= 0; count9--) {
+        console.log(count9)
+    }
+}
